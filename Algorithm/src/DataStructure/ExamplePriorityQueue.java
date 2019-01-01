f5df1a733893b2b6acb025aee3ceea827aa1a676
hakzzang <mbh930312@naver.com>
2019-01-01 15:04:13 +0900

    PriorityQueue

--- Algorithm/src/DataStructure/ExamplePriorityQueue.java
+++ Algorithm/src/DataStructure/ExamplePriorityQueue.java
@@ -1,5 +1,32 @@
 package DataStructure;
 
-public class ExamplePriorityQueue {
+import java.util.PriorityQueue;
+import java.util.Queue;
 
+public class ExamplePriorityQueue {
+	public static void main(String[] args) {
+		PriorityQueue<Integer> queue = new PriorityQueue<Integer>();//큐 생성
+		queue.offer(5);//데이터를 집어 넣음
+		queue.offer(1);//데이터를 집어 넣음
+		queue.offer(2);//데이터를 집어 넣음 
+		
+		queue.offer(3);//데이터를 집어 넣음
+		queue.offer(4);//데이터를 집어 넣음
+		print(queue);//큐를 MIN HEAP으로 SORT해서 출력
+		System.out.println(String.format("poll: %d", queue.poll()));//poll
+		System.out.println(String.format("poll: %d", queue.poll()));//poll
+		System.out.println(String.format("poll: %d", queue.poll()));//poll
+		System.out.println(String.format("poll: %d", queue.poll()));//poll
+		System.out.println(String.format("poll: %d", queue.poll()));//poll
+		
+	}
+	
+	public static void print(Queue<Integer> queue) {
+		System.out.print("data: ");
+		queue.forEach((value)->{
+			System.out.print(value); 
+		});
+		System.out.println(String.format(" size: %d", queue.size())); 
+		
+	}
 }
